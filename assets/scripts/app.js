9c26b0654196e7ae97a77996f3fed54feaa7397c
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-04-20 20:00:15 -0400

    app logic is complete

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,4 @@
+//html elements
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const modalBackdrop = document.getElementById("backdrop");
@@ -5,12 +6,87 @@ const modalCancelButtonEl = addMovieModal.querySelector(".btn--passive");
 const documentBody = document.getElementsByTagName("body");
 const confirmAddMovieButton = modalCancelButtonEl.nextElementSibling;
 const userInputs = document.querySelectorAll("input");
-const alertModal = document.getElementById("alert-modal");
-console.log(userInputs);
+const entryTextSection = document.getElementById("entry-text");
+//logic variables
+const movies = [];
+
+//**** HANDLERS */
+
+//remove movie option from ui
+const handleDeleteMovie = (movieId) => {};
 
 //remove modal backdrop
-const handleCancelModalBackdrop = () => {
+const handleCancelMovieButton = () => {
   toggleMovieModal();
+  clearMovieInputs();
+};
+
+//handle add movie button in add movie modal
+const handleconfirmAddMovieButton = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  //validate input
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please fill out all values. Ratings are between 1-5.");
+  }
+
+  const newMovie = {
+    id: titleValue,
+    title: titleValue,
+    imageUrl: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  toggleMovieModal();
+  clearMovieInputs();
+  renderMovieEl(newMovie.title, newMovie.imageUrl, newMovie.rating);
+  updateUI();
+};
+
+//clears movie inputs in modal
+const clearMovieInputs = () => {
+  for (const userInput of userInputs) {
+    userInput.value = "";
+  }
+};
+
+/*******RENDER TO UI ****/
+
+//renders movie element to movie list section
+const renderMovieEl = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  const listRoot = document.getElementById("movie-list");
+
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+  <div className="movie-element__image">
+    <img src="${imageUrl} alt="${title}">  
+  </div>
+  <div className="movie-element__info">
+  <h2>${title}</h2>
+  <p>${rating}</p>
+  </div>`;
+
+  newMovieElement.addEventListener("click", deleteMovieElement);
+  listRoot.append(newMovieElement);
+};
+
+//add movie elements to update ui
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
 };
 
 //adds modal backdrop
@@ -24,29 +100,11 @@ const toggleMovieModal = () => {
   toggleModalBackDrop();
 };
 
-//handle add movie button in add movie modal
-const handleconfirmAddMovieButton = () => {
-  const title = userInputs[0].value.trim();
-  const imageUrl = userInputs[1].value;
-  const rating = userInputs[2].value;
-
-  //validate input
-  if (
-    title.trim() === "" ||
-    imageUrl.trim() === "" ||
-    rating.trim() === "" ||
-    +rating < 1 ||
-    +rating > 5
-  ) {
-    alert("Please fill out all values. Ratings are between 1-5.");
-  }
-};
-
 //if click outside backdrop it will clear
 modalBackdrop.addEventListener("click", toggleModalBackDrop);
 
 //BUTTONS
-modalCancelButtonEl.addEventListener("click", handleCancelModalBackdrop);
+modalCancelButtonEl.addEventListener("click", handleCancelMovieButton);
 
 //start button to add movie
 startAddMovieButton.addEventListener("click", toggleMovieModal);
